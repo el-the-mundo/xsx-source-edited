017aca1bf011d1ed156e35d3d69062112b539f00
el-the-mundo <130784209+el-the-mundo@users.noreply.github.com>
2023-04-16 21:37:41 +0100

    Update source.lua

--- source.lua
+++ source.lua
@@ -784,7 +784,7 @@ function library:Introduction()
     xsx.BackgroundTransparency = 1.000
     xsx.Size = UDim2.new(0, 80, 0, 21)
     xsx.Font = Enum.Font.Code
-    xsx.Text = "Loading Untitled Hub..."
+    xsx.Text = "Untitled Hub"
     xsx.TextColor3 = Color3.fromRGB(124, 124, 124)
     xsx.TextSize = 10.000
     xsx.TextTransparency = 1
